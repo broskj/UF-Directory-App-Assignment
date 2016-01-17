08fcdaa87d82d551c581f934e3a062571f00ed2c
Kyle Brost <kjbrost@gmail.com>
2016-01-16 23:51:17 -0500

    began writing functions, still needs work

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,37 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config.example.js');
 
 /* Connect to your database */
+mongoose.connect('https://mongolab.com/databases/listings_cen3031_3c');
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
+var newListing = Listing({
 
+});
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+newListing.save(function(err) {
+     if(err) throw err;
+
+     console.log('Listing created');
+});
+
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
+ Listing.find({}, function(err, listings) {
+    if(err) throw err;
+
+    console.log(listings)
+ });
