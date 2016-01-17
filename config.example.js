96525dd2aa2de68ba06606f4f6547d6a957ded08
Kyle Brost <kjbrost@gmail.com>
2016-01-16 23:52:08 -0500

    added mongodb uri

--- config.example.js
+++ config.example.js
@@ -1,10 +1,10 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
 module.exports = {
   db: {
-    uri: '', //place the URI of your mongo database here.
+    uri: 'https://mongolab.com/databases/listings_cen3031_3c', //place the URI of your mongo database here.
   }
 };
 
